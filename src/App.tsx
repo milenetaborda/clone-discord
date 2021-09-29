081c836ad72fd52220f4d6a745cc5884af69520f
Milene Taborda <milene.taborda@provi.com.br>
2021-09-29 10:22:05 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import Layout from './components/Layout';
+import React from "react";
+import Layout from "./components/Layout";
 
-import GlobalStyles from './styles/GlobalStyles';
+import GlobalStyles from "./styles/GlobalStyles";
 
+//test
 function App() {
   return (
     <>
-    <Layout />
+      <Layout />
 
-    <GlobalStyles />
+      <GlobalStyles />
     </>
   );
 }
